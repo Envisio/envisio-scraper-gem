8cbef0f510b43cd0b0ff6787ad8bfc4d7eb92ed3
Jim Li <jli@envisio.com>
2020-08-31 14:20:50 -0700

    Refactor client

--- lib/envisioscraper/api_payload.rb
+++ lib/envisioscraper/api_payload.rb
@@ -1,10 +1,28 @@
 module EnvisioScraper
   class ApiPayload
+    class InvalidPayloadError < StandardError; end
+
     attr_reader :payload
 
     def initialize(payload, payload_signature_keys)
       @payload                = payload
       @payload_signature_keys = payload_signature_keys
+    end
+
+    def self.for_check_status(payload)
+      required_keys = [:id]
+
+      raise InvalidPayloadError.new("Required payload keys: #{required_keys}") if (required_keys - payload.keys).count > 0
+
+      new(payload, required_keys)
+    end
+
+    def self.for_scrape(payload)
+      required_keys = [:scrape_url, :async, :callback_url]
+
+      raise InvalidPayloadError.new("Required payload keys: #{required_keys}") if (required_keys - payload.keys).count > 0
+
+      new(payload, required_keys)
     end
 
     def payload_signature
